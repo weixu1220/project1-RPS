b99471b247efa303357c5d0db965dafd48de484e
Vivian Rice <weixu1220@gmail.com>
2023-06-29 12:43:51 -0600

    animation

--- script.js
+++ script.js
@@ -10,9 +10,17 @@ const gameText = document.getElementById("gameText");
 const round = document.getElementById("round");
 const switchBG = document.getElementById("switchBG")
 const bg = ["../images/bg1.jpg","../images/bg2.jpg","../images/bg3.jpg","../images/bg4.jpg"];
+let playerIcon = document.querySelectorAll('.icon-img');
+let comIcon = document.querySelectorAll('.icon-com');
 let playerOutput;
 let comOutput;
 let name1;
+const p1R = document.getElementById('p1R');
+const p1P = document.getElementById('p1P');
+const p1S = document.getElementById('p1S');
+const p2R = document.getElementById('p2R');
+const p2P = document.getElementById('p2P');
+const p2S = document.getElementById('p2S');
 enter.addEventListener("click",function(){
     cover.style.zIndex = 0;
     name1 = nameInput.value.toUpperCase();
@@ -45,13 +53,15 @@ reset.onclick = function () {
         };
 const computerTurn = () => {
     let outCome = Math.floor(Math.random() * 3);
-    console.log(outCome)
     if(outCome === 0){
             comOutput = "Rock";
+            p2R.classList.add('animate'); 
     }else if(outCome === 1){
             comOutput = "Paper";
+            p2P.classList.add('animate'); 
     }else{
             comOutput = "Scissors";
+            p2S.classList.add('animate'); 
     }
 }
 const winner = () =>{
@@ -70,19 +80,40 @@ const winner = () =>{
     }
 }
 
+//remove class
+const remove1 = () => {
+    for (let i = 0; i < playerIcon.length; i++) {
+        playerIcon[i].classList.remove('animate');
+        }
+}
+const remove2 = () => {
+    for (let i = 0; i < comIcon.length; i++) {
+        comIcon[i].classList.remove('animate');
+        }
+}
 // icons actions
-document.getElementById("p1R").onclick = function() { 
+p1R.onclick = function() {
+    remove1()
+    remove2()
     playerOutput = "Rock";
+    p1R.classList.add('animate');
     round.innerHTML = `${name1} played Rock! `
     winner();
 };   
-document.getElementById("p1P").onclick = function() {  
+p1P.onclick = function() {
+    remove1()
+    remove2()
+    p1P.classList.add('animate');  
     playerOutput = "Paper";
     round.innerHTML = `${name1} played Paper! `;
     winner();
+
 };  
-document.getElementById("p1S").onclick = function() {
+p1S.onclick = function() {
+    remove1()
+    remove2()
+    p1S.classList.add('animate');
     playerOutput = "Scissors";
     round.innerHTML = `${name1} played Scissors! `;
-    winner();  
+    winner();
 };  
